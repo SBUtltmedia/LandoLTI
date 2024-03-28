2286c7250820fed93e7b720716cea08752321cde
AlmondBread <pstdenis@gmail.com>
2024-03-28 17:16:21 -0400

    first commit

---
+++ moodledata/localcache/bootstrap.php
@@ -0,0 +1,8 @@
+<?php
+// ********** This file is generated DO NOT EDIT **********
+$CFG->siteidentifier = 'Y6VXZKF61RHYWA8XACxcAHF9GjSbUh42my-lando-app.lndo.site';
+$CFG->bootstraphash = 'fcdbd45c7d854be1a1beaab682f2604d';
+// Only if the file is not stale and has not been defined.
+if ($CFG->bootstraphash === hash_local_config_cache() && !defined('SYSCONTEXTID')) {
+    define('SYSCONTEXTID', 1);
+}
